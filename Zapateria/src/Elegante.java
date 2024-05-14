47214b16005e12d859c38408a5e86104777ec360
Juan Carlos Pineda <laggart@gmail.com>
2024-05-14 21:02:48 +0200

    Lab done

---
+++ Zapateria/src/Elegante.java
@@ -0,0 +1,6 @@
+public class Elegante extends Zapato {
+    public Elegante(String marca, String modelo, String material, int talla, double precio){
+        super(marca, modelo, material, talla, precio);
+    }
+    
+}
